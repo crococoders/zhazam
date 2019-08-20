6a88678648825d57fc205bd9ef62a82e1ca67f93
Beknar Danabek <beknar.danabek@gmail.com>
2019-08-20 23:11:31 +0600

    chore: init project

---
+++ zhazam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  zhazam
+//
+//  Created by Beknar Danabek on 8/20/19.
+//  Copyright © 2019 Beknar Danabek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
